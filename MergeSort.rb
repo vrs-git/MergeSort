7af7fef984a067d956eb8f15d9537c8c210db9d8
ritika <ritika.pareek@gmail.com>
2015-07-15 22:47:19 -0700

    ygh

---
+++ MergeSort.rb
@@ -0,0 +1,43 @@
+def Merge(array,p,q,r)
+	i = 0
+	
+	h = p
+	j = q+1
+	b = [r+1-p]
+	
+	while h<= q && j <= r
+		if array[h] <= array[j]
+			b[i] = array[h]
+			h = h+1
+		else
+			b[i] = array[j]
+			j = j+1
+		end
+		i++
+	end
+	
+	if h > q 
+		for k in j..r
+			b[i] = array[k]
+			i ++
+		end
+	else
+		for k in h..q
+			b[i] = array[k]
+			i ++
+		end
+	end
+end
+
+def MergeSort(array,p,r)
+	if p < r
+		q = (p +q )
+		MergeSort(array,p,q)
+		MergeSort(array,q+1,r)
+		Merge(array,p,q,r)
+	end
+end
+
+array = [3, 7, 12, 14, 2, 6, 9, 11]
+
+puts MergeSort(array,0,array.length-1)
